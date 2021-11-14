30e59e82ec2149020105213f34a145343bac27ad
ndkv9 <vu.nguyen101@gmail.com>
2021-11-14 17:24:01 +0200

    add test case for Answer can call the handler

--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Answer = ({ country, answer }) => {
-  console.log(country)
   return (
     <p className='answer' onClick={() => answer(country.name.common)}>
       {country.name.common}
--- src/test/Answer.test.js
+++ src/test/Answer.test.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Answer from '../Components/Answer'
+
+const country = {
+  name: {
+    common: 'Finland',
+  },
+}
+const answer = jest.fn()
 
 describe('Answer', () => {
   beforeEach(() => {
-    const country = {
-      name: {
-        common: 'Finland',
-      },
-    }
-    const answer = jest.fn()
     render(<Answer country={country} answer={answer} />)
   })
 
   it('can return the name of the country', () => {
     const countryName = screen.getByText('Finland')
     expect(countryName).toBeInTheDocument()
+  })
+
+  it('can call the callback fn', () => {
+    const countryName = screen.getByText('Finland')
+    fireEvent.click(countryName)
+    expect(answer).toBeCalled()
   })
 })
